cca9813ae13d628c8f3e504d0140decada00036f
mOatles <tehmoat@gmail.com>
2018-01-09 18:57:17 -0800

    End the texlister madness once and for all

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Texlister.Properties {
+namespace Lumenati.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Texlister.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Texlister.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Lumenati.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Texlister.Properties {
+namespace Lumenati.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
